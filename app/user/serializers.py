0c314e716f85431ccf8dee78ed481fe22a6cd925
Japhet Omorodion <japhet.omorodion@gmail.com>
2020-01-13 19:46:39 +0100

    Token generation feature added

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -27,9 +27,28 @@ class AuthTokenSerializer(serializers.Serializer):
     """
     Serializer for the user authentication object
     """
-    email = serializers.Charfield()
+    email = serializers.CharField()
     password = serializers.CharField(
         style={'input_type': 'password'},
         trim_whitespace=False
     )
+    
+    def validate(self, attrs):
+        """
+        Validate and authenticate user
+        """
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        user = authenticate(
+            request= self.context.get('request'),
+            username= email,
+            password=password
+        )
+        
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authenticate')
+        attrs['user'] = user
+        return attrs
     
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
-# Create your views here.
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.settings import api_settings
 from user.serializers import UserSerializer
 
 
@@ -8,3 +10,12 @@ class CreateUserView(generics.CreateAPIView):
     create a new user in the system
     """
     serializer_class = UserSerializer
+    
+    
+class CreateTokenView(ObtainAuthToken):
+    """
+    Create a new auth token for user
+    """
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
