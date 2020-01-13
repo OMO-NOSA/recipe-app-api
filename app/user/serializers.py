b9b6d5007dba5de4f0b2db2bf460cf1267d04616
Japhet Omorodion <japhet.omorodion@gmail.com>
2020-01-13 12:37:06 +0100

    Edited README.md file

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,4 +1,7 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
+
+
 from rest_framework import serializers
 
 
@@ -18,3 +21,15 @@ class UserSerializer(serializers.ModelSerializer):
             """
             return get_user_model().objects.create_user(**validated_data)
         
+        
+        
+class AuthTokenSerializer(serializers.Serializer):
+    """
+    Serializer for the user authentication object
+    """
+    email = serializers.Charfield()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+    
